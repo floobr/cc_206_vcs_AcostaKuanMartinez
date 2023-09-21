57f281292d58e2f1d51e37d9d83cdaec69661d54
James Martinez <james.martinez0402@gmail.com>
2023-09-21 22:07:10 +0800

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cc_206_vcs_acosta_kuan_martinez
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/cc_206_vcs_acosta_kuan_martinez/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cc_206_vcs_acosta_kuan_martinez
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
